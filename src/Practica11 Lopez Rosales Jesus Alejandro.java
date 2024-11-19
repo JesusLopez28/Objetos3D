871723690c59479ffa78794737892275e1506c5c
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-11-19 10:38:27 -0600

    Initial commit

--- src/Practica11 Lopez Rosales Jesus Alejandro.java
+++ src/Practica11 Lopez Rosales Jesus Alejandro.java
@@ -1,4 +1,4 @@
-import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.geometry.Cylinder;
 import com.sun.j3d.utils.geometry.Box;
 import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.universe.SimpleUniverse;
@@ -16,9 +16,9 @@ class Objetos3D extends JFrame {
     private Timer rotationTimer;
     private double rotSpeedX = 0.02;
     private double rotSpeedY = 0.02;
-    private double bounceHeight = 1; // Altura máxima del rebote
-    private double bounceSpeed = 0.05; // Velocidad del rebote
-    private double bouncePosition = 0; // Posición actual del rebote
+    private double bounceHeight = 1;
+    private double bounceSpeed = 0.05;
+    private double bouncePosition = 0;
 
     public Objetos3D() {
         configurarVentana();
@@ -82,19 +82,20 @@ class Objetos3D extends JFrame {
 
         transformGroupSphere = new TransformGroup();
         transformGroupSphere.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-        Sphere sphere = new Sphere(0.3f, Sphere.GENERATE_NORMALS | Sphere.GENERATE_TEXTURE_COORDS, 50,
+        Cylinder cylinder = new Cylinder(0.2f, 0.6f, Cylinder.GENERATE_NORMALS | Cylinder.GENERATE_TEXTURE_COORDS,
                 createChromeMaterial(new Color3f(0.9f, 0.1f, 0.6f))
         );
 
-        Transform3D spherePosition = new Transform3D();
-        spherePosition.setTranslation(new Vector3f(0.8f, 0.0f, -2.0f));
-        transformGroupSphere.setTransform(spherePosition);
-        transformGroupSphere.addChild(sphere);
+        Transform3D cylinderPosition = new Transform3D();
+        cylinderPosition.setTranslation(new Vector3f(0.8f, 0.0f, -2.0f));
+        transformGroupSphere.setTransform(cylinderPosition);
+        transformGroupSphere.addChild(cylinder);
         root.addChild(transformGroupSphere);
 
         root.compile();
         return root;
     }
+
 
     private void setupLighting(BranchGroup root) {
         BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
